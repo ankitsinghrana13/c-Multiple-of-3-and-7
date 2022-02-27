9ff8389bc813293ae223998c0c16a779cf739417
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:15:22 +0800

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+
+            Console.WriteLine("enter the your number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a%3 == 0 && a%7 == 0)
+            {
+                Console.WriteLine("Multiple of 3 and 7");
+            }
+
+            else
+            {
+                Console.WriteLine("not Multiple of any given  digits");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
